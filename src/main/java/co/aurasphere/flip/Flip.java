15a61d8cd738816fef9482e9babc5715471b45d7
Donato Rimenti <donatohan.rimenti@gmail.com>
2017-09-22 17:54:29 +0200

    Added javadoc and cleaned up.

--- src/main/java/co/aurasphere/flip/Flip.java
+++ src/main/java/co/aurasphere/flip/Flip.java
@@ -1,22 +1,43 @@
 package co.aurasphere.flip;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import sun.misc.Unsafe;
+import sun.reflect.ReflectionFactory;
 
+/**
+ * Main class of Flip library.
+ * 
+ * @author Donato Rimenti
+ *
+ */
+@SuppressWarnings({ "restriction", "unchecked" })
 public class Flip {
 
+	/**
+	 * Logger.
+	 */
 	private final static Logger LOG = LoggerFactory.getLogger(Flip.class);
 
+	/**
+	 * Unsafe used for Flip operations.
+	 */
 	private static Unsafe unsafe;
 
+	/**
+	 * Static initializer.
+	 */
 	static {
 		init();
 	}
 
+	/**
+	 * Initializes Flip by loading the {@link #unsafe}.
+	 */
 	private static void init() {
 		Field theUnsafe;
 		try {
@@ -24,11 +45,18 @@ public class Flip {
 			theUnsafe.setAccessible(true);
 			unsafe = (Unsafe) theUnsafe.get(null);
 		} catch (ReflectiveOperationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOG.error("Error while instantiating class {} without constructor", e);
 		}
 	}
 
+	/**
+	 * Instantiates a class without calling any constructor.
+	 * 
+	 * @param T
+	 *            the class to instantiate
+	 * @return an instance of that class
+	 * @throws FlipException
+	 */
 	public static <T> T instantiateWithoutConstructor(Class<T> T) throws FlipException {
 		try {
 			return (T) unsafe.allocateInstance(T);
@@ -37,9 +65,13 @@ public class Flip {
 			throw new FlipException("Error while instantiating class {} without constructor", e);
 		}
 	}
-	
-	public static <T> T defineClass(){
-		unsafe.define
-	}
+
+//	public static <T, E> T injectConstructor(Class<T> T, Constructor<E> constructor) {
+//		Constructor<T> silentConstructor = ReflectionFactory.getReflectionFactory().newConstructorForSerialization(T,
+//				constructor);
+//		silentConstructor.setAccessible(true);
+//		T instance = silentConstructor.newInstance();
+//		return null;
+//	}
 
 }
--- src/main/java/co/aurasphere/flip/FlipException.java
+++ src/main/java/co/aurasphere/flip/FlipException.java
@@ -1,25 +1,46 @@
 package co.aurasphere.flip;
 
+/**
+ * Exception class for Flip library.
+ * 
+ * @author Donato Rimenti
+ *
+ */
 public class FlipException extends Exception {
 
+	/**
+	 * The Constant serialVersionUID.
+	 */
 	private static final long serialVersionUID = 1L;
 
-	public FlipException() {
-		super();
-	}
-
-	public FlipException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
+	/**
+	 * Instantiates a new FlipException.
+	 *
+	 * @param message
+	 *            the message.
+	 * @param cause
+	 *            the cause.
+	 */
 	public FlipException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Instantiates a new FlipException.
+	 *
+	 * @param message
+	 *            the message.
+	 */
 	public FlipException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Instantiates a new FlipException.
+	 *
+	 * @param cause
+	 *            the cause.
+	 */
 	public FlipException(Throwable cause) {
 		super(cause);
 	}
